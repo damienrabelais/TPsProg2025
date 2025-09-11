f53fdd77a56ee210e216e8e0f5cc0dcb602247d5
damienrabelais <damien.boucard@ac-rennes.fr>
2025-09-11 14:28:36 +0200

    TP07_1

---
+++ TPsProg2025/TP07_1.vb
@@ -0,0 +1,23 @@
+﻿Module TP07_1
+
+    Sub Main()
+        Dim montantSaisi, remise, montantNet As Double
+        Console.WriteLine("Veuillez taper votre montant")
+        montantSaisi = Console.ReadLine()
+        If montantSaisi < 2000 Then
+            remise = 0
+            Console.WriteLine("Pas de remise")
+        ElseIf montantSaisi <= 5000 Then
+            remise = montantSaisi * 1 / 100
+            Console.WriteLine("Remise de 1%")
+        Else
+            remise = montantSaisi * 2 / 100
+            Console.WriteLine("Remise de 2%")
+        End If
+        montantNet = montantSaisi - remise
+        Console.WriteLine("Le montant net est : " + montantNet.ToString())
+        Console.WriteLine("Au revoir")
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ TPsProg2025/Test.vb
@@ -0,0 +1,21 @@
+﻿Module Test
+
+    Sub Main()
+        Dim n As Integer
+        Dim compteur As Double
+        ' Saisir avant de rentrer (ou non) dans la boucle !
+        'Console.WriteLine("Entrez un nombre (0 pour fin) : ")
+        'n = Console.ReadLine()
+        n = 1
+        While n <> 0   ' boucle de saisie des nombres
+            'Console.WriteLine("Le carré vaut : " + (n * n).ToString)
+            compteur = compteur + 1
+            Console.WriteLine(compteur)
+            'Console.WriteLine("Entrez un nombre (0 pour fin) : ")
+            'n = Console.ReadLine()
+        End While
+        Console.WriteLine("n  = 0, au revoir")
+        Console.ReadLine()
+    End Sub
+
+End Module
