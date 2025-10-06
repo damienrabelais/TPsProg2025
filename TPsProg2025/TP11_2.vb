67226f9a2d3871af077bec6ed4d888fa9d2d096d
damienrabelais <damien.boucard@ac-rennes.fr>
2025-10-06 11:02:59 +0200

    TP11_2 Factorielle

---
+++ TPsProg2025/TP11_2.vb
@@ -0,0 +1,27 @@
+﻿Module TP11_2
+    Function Factorielle(ByVal pN As Integer) As Double
+        Dim resultat As Double = 1
+        Dim i As Integer
+        For i = 1 To pN
+            resultat = resultat * i
+        Next
+        Return resultat
+    End Function
+    Sub main()
+        Dim n As Integer
+        Do
+            Do
+                Console.WriteLine("Saisir un nombre n >= 0 ou n=-1 pour sortir")
+                n = Console.ReadLine()
+                If n < 0 And n <> -1 Then
+                    Console.WriteLine("n >= 0 ou n=-1 pour sortir")
+                End If
+            Loop Until n >= 0 Or n = -1
+            If n <> -1 Then
+                Console.WriteLine("La factorielle de ce nombre est " + Factorielle(n).ToString())
+            End If
+        Loop Until n = -1
+        Console.WriteLine("Au revoir")
+        Console.ReadLine()
+    End Sub
+End Module
--- TPsProg2025/Test.vb
+++ TPsProg2025/Test.vb
@@ -1,47 +1,7 @@
 ﻿Module Test
 
-    Function PérimètreCercle(ByVal pRayon As Double) As Double
-        Dim perimetre As Double
-        perimetre = 2 * Math.PI * pRayon
-        Return perimetre
-        'Return 2 * Math.PI * pRayon
-    End Function
-
-
 
     Sub Main() ' Programme principal
-        Dim choix As Integer
-        Dim rayon As Double
-        Do
-            Console.WriteLine("1. Calcul du périmètre d'un cercle.")
-            Console.WriteLine("2. Calcul de la surface d'un cercle.")
-            Console.WriteLine("3. Calcul du périmètre d'un rectangle.")
-            Console.WriteLine("4. Calcul de la surface d'un rectangle.")
-            Console.WriteLine("5. Quitter.")
-            Console.WriteLine("Choix ?")
-            choix = Console.ReadLine()
 
-            Select Case choix
-                Case 1
-                    Console.WriteLine("1. Calcul du périmètre d'un cercle.")
-                    Do
-                        Console.WriteLine("Rayon ?")
-                        rayon = Console.ReadLine()
-                        If rayon <= 0 Then
-                            Console.WriteLine("Rayon > 0")
-                        End If
-                    Loop Until rayon > 0
-                    Console.WriteLine("Périmètre: " + PérimètreCercle(rayon).ToString())
-                Case 2
-                    Console.WriteLine("2. Calcul de la surface d'un cercle.")
-                Case 5
-                    Console.WriteLine("Au revoir")
-                Case Else
-                    Console.WriteLine("Erreur de saisie")
-            End Select
-        Loop Until choix = 5
-
-
-        Console.ReadLine()
     End Sub ' Fin du main
 End Module
